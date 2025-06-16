450f67c98b3c869cde76115668201666a481e0f2
Gabi22-ctrl <gabs1507929@gmail.com>
2025-06-15 22:53:02 -0600

    error en las funciones arreglado

--- src/chardev.h
+++ src/chardev.h
@@ -2,15 +2,14 @@
 #define CHARDEV_H
 
 int init_chardev(void);
-
 void cleanup_chardev(void);
 
 //ssize_t es un tipo de dato para representar un numero de bytes o indicar un error si es negativo
 
 //Funcion para leer el buffer
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset);
+ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset);
 
-//Funcion para escribir en el buffer
-static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset);
+//Funcion para escribir en el buffer, las funciones no deben ser static porque se usan en otros archivos
+ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset);
 
 #endif
