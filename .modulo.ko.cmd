09ad2bb60156b9e70222f6c0eab6838a419eb238
andxsmz <michael06amartinez@gmail.com>
2025-06-19 03:40:55 +0000

    Modificación de la función dev_read en chardev.c

---
+++ .modulo.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/Andres/ProyectoIE-0117/modulo.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T scripts/module.lds -o /home/Andres/ProyectoIE-0117/modulo.ko /home/Andres/ProyectoIE-0117/modulo.o /home/Andres/ProyectoIE-0117/modulo.mod.o
---
+++ modulo.mod
@@ -0,0 +1,3 @@
+/home/Andres/ProyectoIE-0117/src/modulo.o
+/home/Andres/ProyectoIE-0117/src/chardev.o
+/home/Andres/ProyectoIE-0117/src/last.o
