e99efa42b8e075503d1c43bea1901cc5371f91a1
andxsmz <michael06amartinez@gmail.com>
2025-06-19 04:34:18 +0000

    Delete files

--- .modulo.o.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/Andres/ProyectoIE-0117/modulo.o := ld -m elf_x86_64 -z noexecstack --no-warn-rwx-segments   -r -o /home/Andres/ProyectoIE-0117/modulo.o @/home/Andres/ProyectoIE-0117/modulo.mod 
