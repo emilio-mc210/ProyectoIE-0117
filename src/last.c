5c42513f939c27551b360b4de510a034e5cb4cad
andxsmz <michael06amartinez@gmail.com>
2025-07-03 04:54:08 +0000

    Documentación del código

--- src/last.c
+++ src/last.c
@@ -1,17 +1,21 @@
-//Para encontrar el ultimo mensaje
-#include <linux/slab.h> //Para kstrdup()
-#include <linux/string.h>
+//Archivo para gestionar el almacenamiento y recuperación del último mensaje en el char device
+
+#include <linux/slab.h> //Para kstrdup() y kfree()
+#include <linux/string.h> //Para funciones de manejo de strings
 #include "last.h"
 
 //Variable para este archivo que guarda el ultimo mensaje
 static char *last_msg = NULL;
 
+//Almacena una copia del mensjae como el último recibido
 void set_last_message(const char *msg) {
     kfree(last_msg);
 
-    last_msg = kstrdup(msg, GFP_KERNEL);//Duplica un string usando memoria dinamica en kernel 
+    //Duplica un string usando memoria dinamica en kernel 
+    last_msg = kstrdup(msg, GFP_KERNEL);
 }
 
+//Recupera el último mensaje almacenado
 const char* get_last_message(void) {
     return last_msg;
 }
