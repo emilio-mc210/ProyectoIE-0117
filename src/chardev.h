6b94d56b2946168624ae1ff4e30a21aaef9580e8
andxsmz <michael06amartinez@gmail.com>
2025-07-03 03:18:17 +0000

    Documentación de chardev.h

--- src/chardev.h
+++ src/chardev.h
@@ -1,24 +1,33 @@
+/*Header CHARDEV_H para el driver de dispositivo de caracteres
+*Se definen constantes de configuración y funciones
+*DEVICE_NAME: define nombre del dispositivo que aparecerá en /dev y /proc/devices
+*ENTRY_SIZE: define el tamaño máximo en bytes para cada entrada del buffer 
+*MAX_ENTRIES: define la capacidad máxima de mensjaes en el buffer circular
+ */
 #ifndef CHARDEV_H
 #define CHARDEV_H
-#define DEVICE_NAME "chardev" //Nombre del char device como aparece en /proc/devices
-#define ENTRY_SIZE 128 //Tamaño máximo en bytes que puede tener cada entrada en el buffer 
-#define MAX_ENTRIES 10 //Capacidad máxima de entradas que puede almacenar el buffer
+#define DEVICE_NAME "chardev" 
+#define ENTRY_SIZE 128 
+#define MAX_ENTRIES 10
 
+//Función para inicializar y registrar el dispositivo 
 int init_chardev(void);
+
+//Función para liberar todos los recursos del dispositivo
 void cleanup_chardev(void);
 
+//Función para limpiar todas las entradas del dispositivo
 void clear_chardev(void); 
 
-//ssize_t es un tipo de dato para representar un numero de bytes o indicar un error si es negativo
-
-//Funcion para leer el buffer
+//Funcion para leer el dispositivo
 ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset);
 
-//Funcion para escribir en el buffer, las funciones no deben ser static porque se usan en otros archivos
+//Funcion para escribir en el dispositivo
 ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset);
 
 //Funcion para abrir el dispositivo
 int dev_open(struct inode *inode, struct file *filep);
+
 //Funcion para cerrar el dispositivo/archivo
 int dev_release(struct inode *inode, struct file *filep);
 
